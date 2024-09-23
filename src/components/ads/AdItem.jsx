f350c866eaa5a45b50008a5402fb254004d32de5
Yasin Soley <Yasin.dev.dg@gmail.com>
2024-09-23 14:38:17 +0330

    finish AdList.jsx and AdItem.jsx

--- src/components/ads/AdItem.jsx
+++ src/components/ads/AdItem.jsx
@@ -1,3 +1,14 @@
-export default function AdItem() {
-	return <div>AdItem</div>
+import { Link } from 'react-router-dom'
+
+/* eslint-disable react/prop-types */
+export default function AdItem({ data }) {
+    const { id, address } = data
+
+    return (
+        <Link
+            to={`/ads/${id}`}
+            className='p-5 border-2 rounded-md shadow-md flex-1 text-center'>
+            {address}
+        </Link>
+    )
 }
--- src/components/ads/AdList.jsx
+++ src/components/ads/AdList.jsx
@@ -1,3 +1,12 @@
-export default function AdList() {
-	return <div>AdList</div>
+import AdItem from './AdItem'
+
+/* eslint-disable react/prop-types */
+export default function AdList({ data }) {
+    return (
+        <div className='flex gap-x-4'>
+            {data.map((item) => {
+                return <AdItem key={item.id} data={item} />
+            })}
+        </div>
+    )
 }
